44aa6181adbb8e759f067e21c578355c8144c097
Omar Salem <omar.gamil.salem@gmail.com>
2020-06-15 12:56:47 +1000

    init

---
+++ src/test/java/org/omarsalem/zombies/AppTest.java
@@ -0,0 +1,20 @@
+package org.omarsalem.zombies;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
